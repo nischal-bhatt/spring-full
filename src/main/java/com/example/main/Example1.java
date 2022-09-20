006ff61b7654180226bd7b4fb53d48166d146ac7
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-09-20 14:25:37 +0800

    naming beans using @Bean method

---
+++ src/main/java/com/example/main/Example1.java
@@ -0,0 +1,42 @@
+package com.example.main;
+
+import com.example.beans.Vehicle;
+import com.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+
+    	// here, we are using the new keyword, 
+    	// this object is not managed by the spring framework
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
+        /*
+        The var keyword was introduced in Java 10. Type inference is used in
+        var keyword in which it detects automatically the datatype of a variable
+        based on the surrounding context.
+        * */
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        /*
+        We don’t need to do any explicit casting while fetching a bean from context.
+        Spring is smart enough to look for a bean of the type you requested in its context.
+        If such a bean doesn’t exist,Spring will throw an exception.
+        * */
+        
+        //we can tell the context which bean to retrieve
+        // by passing the first value as the name of the
+        //method in the config --> in this case hellohi
+        
+        String hello = context.getBean("hello",String.class);
+        System.out.println("String value from Spring Context is: " + hello);
+        Integer num = context.getBean(Integer.class);
+        System.out.println("Integer value from Spring Context is: " + num);
+
+    }
+}
